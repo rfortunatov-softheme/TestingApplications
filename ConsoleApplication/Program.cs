896ce8ec7c33fc21aa469a0890193a939c3d725d
Roman Fortunatov <roman.fortunatov@softheme.com>
2015-06-20 17:09:48 +0300

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TapesDatabaseClientTests.TapesDatabaseClientTests();
+            test.TestClient();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
