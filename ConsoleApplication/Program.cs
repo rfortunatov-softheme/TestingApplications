8eafd79df2205817d1c1194f583baa81b469730a
Roman Fortunatov <roman.fortunatov@softheme.com>
2016-03-04 11:21:30 +0200

    StopTester

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,36 +1,208 @@
-﻿using System;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace TestApplication
 {
-    public class TestCL
-    {
-        public string onr { get; set; }
-
-        public string two { get; set; }
-    }
-
     class Program
     {
-        private static TestCL _prop;
-
-        private static TestCL prop
+        [Flags]
+        public enum ACCESS_MASK : uint
         {
-            get
-            {
-                Console.WriteLine("Getting");
-                return _prop;
-            }
-            set
-            {
-                Console.WriteLine("Setting");
-                _prop = value;
-            }
+            DELETE = 0x00010000,
+            READ_CONTROL = 0x00020000,
+            WRITE_DAC = 0x00040000,
+            WRITE_OWNER = 0x00080000,
+            SYNCHRONIZE = 0x00100000,
+
+            STANDARD_RIGHTS_REQUIRED = 0x000F0000,
+
+            STANDARD_RIGHTS_READ = 0x00020000,
+            STANDARD_RIGHTS_WRITE = 0x00020000,
+            STANDARD_RIGHTS_EXECUTE = 0x00020000,
+
+            STANDARD_RIGHTS_ALL = 0x001F0000,
+
+            SPECIFIC_RIGHTS_ALL = 0x0000FFFF,
+
+            ACCESS_SYSTEM_SECURITY = 0x01000000,
+
+            MAXIMUM_ALLOWED = 0x02000000,
+
+            GENERIC_READ = 0x80000000,
+            GENERIC_WRITE = 0x40000000,
+            GENERIC_EXECUTE = 0x20000000,
+            GENERIC_ALL = 0x10000000,
+
+            DESKTOP_READOBJECTS = 0x00000001,
+            DESKTOP_CREATEWINDOW = 0x00000002,
+            DESKTOP_CREATEMENU = 0x00000004,
+            DESKTOP_HOOKCONTROL = 0x00000008,
+            DESKTOP_JOURNALRECORD = 0x00000010,
+            DESKTOP_JOURNALPLAYBACK = 0x00000020,
+            DESKTOP_ENUMERATE = 0x00000040,
+            DESKTOP_WRITEOBJECTS = 0x00000080,
+            DESKTOP_SWITCHDESKTOP = 0x00000100,
+
+            WINSTA_ENUMDESKTOPS = 0x00000001,
+            WINSTA_READATTRIBUTES = 0x00000002,
+            WINSTA_ACCESSCLIPBOARD = 0x00000004,
+            WINSTA_CREATEDESKTOP = 0x00000008,
+            WINSTA_WRITEATTRIBUTES = 0x00000010,
+            WINSTA_ACCESSGLOBALATOMS = 0x00000020,
+            WINSTA_EXITWINDOWS = 0x00000040,
+            WINSTA_ENUMERATE = 0x00000100,
+            WINSTA_READSCREEN = 0x00000200,
+
+            WINSTA_ALL_ACCESS = 0x0000037F
+        }
+
+        [Flags]
+        public enum SERVICE_CONTROL : uint
+        {
+            STOP = 0x00000001,
+            PAUSE = 0x00000002,
+            CONTINUE = 0x00000003,
+            INTERROGATE = 0x00000004,
+            SHUTDOWN = 0x00000005,
+            PARAMCHANGE = 0x00000006,
+            NETBINDADD = 0x00000007,
+            NETBINDREMOVE = 0x00000008,
+            NETBINDENABLE = 0x00000009,
+            NETBINDDISABLE = 0x0000000A,
+            DEVICEEVENT = 0x0000000B,
+            HARDWAREPROFILECHANGE = 0x0000000C,
+            POWEREVENT = 0x0000000D,
+            SESSIONCHANGE = 0x0000000E
+        }
+
+        public enum SERVICE_STATE : uint
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007
+        }
+
+        [Flags]
+        public enum SERVICE_ACCEPT : uint
+        {
+            STOP = 0x00000001,
+            PAUSE_CONTINUE = 0x00000002,
+            SHUTDOWN = 0x00000004,
+            PARAMCHANGE = 0x00000008,
+            NETBINDCHANGE = 0x00000010,
+            HARDWAREPROFILECHANGE = 0x00000020,
+            POWEREVENT = 0x00000040,
+            SESSIONCHANGE = 0x00000080,
+        }
+        
+        [Flags]
+        internal enum SERVICE_TYPE : int
+        {
+            SERVICE_KERNEL_DRIVER = 0x00000001,
+            SERVICE_FILE_SYSTEM_DRIVER = 0x00000002,
+            SERVICE_WIN32_OWN_PROCESS = 0x00000010,
+            SERVICE_WIN32_SHARE_PROCESS = 0x00000020,
+            SERVICE_INTERACTIVE_PROCESS = 0x00000100
+        }
+
+        [Flags]
+        internal enum CONTROLS_ACCEPTED : int
+        {
+            SERVICE_ACCEPT_NETBINDCHANGE = 0x00000010,
+            SERVICE_ACCEPT_PARAMCHANGE = 0x00000008,
+            SERVICE_ACCEPT_PAUSE_CONTINUE = 0x00000002,
+            SERVICE_ACCEPT_PRESHUTDOWN = 0x00000100,
+            SERVICE_ACCEPT_SHUTDOWN = 0x00000004,
+            SERVICE_ACCEPT_STOP = 0x00000001,
+
+            //supported only by HandlerEx
+            SERVICE_ACCEPT_HARDWAREPROFILECHANGE = 0x00000020,
+            SERVICE_ACCEPT_POWEREVENT = 0x00000040,
+            SERVICE_ACCEPT_SESSIONCHANGE = 0x00000080,
+            SERVICE_ACCEPT_TIMECHANGE = 0x00000200,
+            SERVICE_ACCEPT_TRIGGEREVENT = 0x00000400,
+            SERVICE_ACCEPT_USERMODEREBOOT = 0x00000800
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct SERVICE_STATUS
+        {
+            public SERVICE_TYPE dwServiceType;
+            public SERVICE_STATE dwCurrentState;
+            public CONTROLS_ACCEPTED dwControlsAccepted;
+            public long dwWin32ExitCode;
+            public long dwServiceSpecificExitCode;
+            public long dwCheckPoint;
+            public long dwWaitHint;
+        }
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(string machineName, string databaseName, uint dwAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ControlService(IntPtr hService, SERVICE_CONTROL dwControl, ref SERVICE_STATUS lpServiceStatus);
+
+        [DllImport("advapi32.dll", EntryPoint = "QueryServiceStatus", CharSet = CharSet.Auto)]
+        public static extern bool QueryServiceStatus(IntPtr hService, ref SERVICE_STATUS dwServiceStatus);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseServiceHandle(IntPtr hSCObject);
+
+        public static void CH(string str)
+        {
+            str = "FUCK!";
         }
 
         static void Main(string[] args)
         {
-            prop = new TestCL();
-            prop.onr = "fuck";
+            var currentState = SERVICE_STATE.SERVICE_RUNNING;
+            var schm = OpenSCManager("10.10.11.186", null, (uint)ACCESS_MASK.MAXIMUM_ALLOWED);  //connect to the service control manager
+            var status = new SERVICE_STATUS();
+            if (schm != IntPtr.Zero)
+            {
+                var schs = OpenService(schm, "DellRdsSvc", (uint) SERVICE_CONTROL.STOP);
+                    // open service handle, start and query status
+                if (schs != IntPtr.Zero)
+                {
+                    if (ControlService(schs, SERVICE_CONTROL.STOP, ref status))
+                    {
+                        if (QueryServiceStatus(schs, ref status))
+                        {
+                            while (SERVICE_STATE.SERVICE_RUNNING != currentState)
+                            {
+                                var checkPoint = status.dwCheckPoint;
+                                //dwCheckPoint contains a value incremented periodically to report progress of a long operation.  Store it.
+                                Thread.Sleep(10); //Sleep for recommended time before checking status again
+                                if (!QueryServiceStatus(schs, ref status))
+                                {
+                                    break; //couldn't check status
+                                }
+
+                                if (status.dwCheckPoint < checkPoint)
+                                {
+                                    break; //if QueryServiceStatus didn't work for some reason, avoid infinite loop
+                                }
+                            } //while not running
+
+                            CloseServiceHandle(schs);
+                        }
+                    } //if able to get service handle
+
+                    CloseServiceHandle(schm);
+                } //if able to get svc mgr handle
+                Console.WriteLine(currentState); //if we were able to start it, return true
+            }
+
             Console.WriteLine("Done");
             Console.ReadKey();
         }
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,136 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <ctime>
+
+void StopService(const wchar_t * serviceName, const wchar_t * hostName)
+{
+    printf("Stopping service %ws \n", serviceName);
+    SC_HANDLE serviceManager = OpenSCManager(hostName, NULL, GENERIC_ALL);
+    SERVICE_STATUS status;
+    if (serviceManager)
+    {
+        SC_HANDLE service = OpenService(serviceManager, serviceName, SC_MANAGER_ALL_ACCESS);
+        if (service)
+        {
+            while (QueryServiceStatus(service, &status))
+            {
+                if (status.dwCurrentState == SERVICE_RUNNING)
+                {
+                    break;
+                }
+
+                Sleep(100);
+            }
+
+            clock_t start = clock(), diff;            
+            
+            if (!ControlService(service, SERVICE_CONTROL_STOP, &status))
+            {
+                QueryServiceStatus(service, &status);
+                diff = clock() - start;
+
+                int msec = diff * 1000 / CLOCKS_PER_SEC;
+                printf("Time taken %d seconds %d milliseconds\n", msec / 1000, msec % 1000);
+            }
+            else
+            {                
+                if (QueryServiceStatus(service, &status))
+                {
+                    while (SERVICE_STOPPED != status.dwCurrentState)
+                    {
+                        DWORD checkPoint = status.dwCheckPoint;
+                        printf("Service stop checkpoint is: %d", checkPoint);
+                        //dwCheckPoint contains a value incremented periodically to report progress of a long operation.  Store it.
+                        Sleep(21000); //Sleep for recommended time before checking status again                        
+                        if (!QueryServiceStatus(service, &status))
+                        {
+                            break; //couldn't check status
+                        }
+
+                        if (status.dwCheckPoint < checkPoint)
+                        {
+                            break; //if QueryServiceStatus didn't work for some reason, avoid infinite loop
+                        }
+                    } //while not running
+
+                    diff = clock() - start;
+
+                    int msec = diff * 1000 / CLOCKS_PER_SEC;
+                    printf("Time taken %d seconds %d milliseconds\n", msec / 1000, msec % 1000);
+                    CloseServiceHandle(service);
+                }
+            } //if able to get service handle
+
+            CloseServiceHandle(serviceManager);
+        } //if able to get svc mgr handle
+    }
+}
+
+void StartService(const wchar_t * serviceName, const wchar_t * hostName)
+{
+    SC_HANDLE serviceManager = OpenSCManager(hostName, NULL, GENERIC_ALL);
+    SERVICE_STATUS status;
+    if (serviceManager)
+    {
+        SC_HANDLE service = OpenService(serviceManager, serviceName, SC_MANAGER_ALL_ACCESS);
+        if (service)
+        {
+            while (QueryServiceStatus(service, &status))
+            {
+                if (status.dwCurrentState == SERVICE_STOPPED)
+                {
+                    break;
+                }
+
+                Sleep(100);
+            }
+
+            if (!ControlService(service, SERVICE_CONTROL_CONTINUE, &status) && GetLastError() != 1053)
+            {
+                QueryServiceStatus(service, &status);
+            }
+            else
+            {
+                if (QueryServiceStatus(service, &status))
+                {
+                    while (SERVICE_RUNNING != status.dwCurrentState)
+                    {
+                        DWORD checkPoint = status.dwCheckPoint;
+                        //dwCheckPoint contains a value incremented periodically to report progress of a long operation.  Store it.
+                        Sleep(10); //Sleep for recommended time before checking status again
+                        if (!QueryServiceStatus(service, &status))
+                        {
+                            break; //couldn't check status
+                        }
+
+                        if (status.dwCheckPoint < checkPoint)
+                        {
+                            break; //if QueryServiceStatus didn't work for some reason, avoid infinite loop
+                        }
+                    } //while not running
+
+                    CloseServiceHandle(service);
+                }
+            } //if able to get service handle
+
+            CloseServiceHandle(serviceManager);
+        } //if able to get svc mgr handle
+    }
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    const wchar_t * machineName = _T("10.10.3.2");
+    const wchar_t * logon_user = _T("Administrator");
+    const wchar_t * password = _T("123asdQ");
+    const wchar_t * domain = _T("");
+    HANDLE token = NULL;
+    LogonUser(logon_user, NULL, password, LOGON32_LOGON_NEW_CREDENTIALS, LOGON32_PROVIDER_WINNT40, &token);
+    ImpersonateLoggedOnUser(token);
+    
+    StopService(L"DellRdsSvc", machineName);    
+    return 0;
+}
